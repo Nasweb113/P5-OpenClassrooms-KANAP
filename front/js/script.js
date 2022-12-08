c64549cf5087bb4224214388dc15d1900209651c
Nasweb113 <104895169+Nasweb113@users.noreply.github.com>
2022-12-08 13:33:44 +0100

    updated

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,37 @@
-fetch("http://localhost:3000/api/products") //using fetch to access URL (used json placeholder template)
-.then((res) => res.json()) // 
-.then((info) => addProducts(info))
+//first we FETCH the info
+//using fetch to access URL (used json placeholder template)
+// placeholder text fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => console.log(json))
 
 
-
-function addProducts(info) {
-    const imageUrl = info[0].imageUrl //access the first element in the list of URLs
-//creation of the requested <a> element 
-    const anchor = document.createElement("a")  //create an element "let element = document.createElement(tagName[, options]);"
-    anchor.href = imageUrl
-    anchor.text = "a great Kanap!"
-
-    const items = document.querySelector("#items") // ID from HTML, ID better as it is more specific
-    {
-        items.appendChild(anchor)
+    fetch("http://localhost:3000/api/products") //using fetch to access URL (used json placeholder template)
+    .then((res) => res.json()) // 
+    .then((info) => addProducts(info)) 
+    //it fetches the info and passes by Add products-->
+    //it recoveres the info from index 0 in the array 
+    //it calls the makeAnchor function and calls the image url and creates the <a></a> anchor element
+    //recovers the image from the first line, index 0
+    //then it puts the infor in the variable anchor
+    //then it passes by appendChildren and takes the sent element and attaches it to #items element
+    //once it finds the id #items, it adds the anchor 
+    
+    
+    function addProducts(info) {
+        
+        const imageUrl = info[0].imageUrl //access the first element in the list of URLs
+       
+        
+       const anchor = makeAnchor(imageUrl); //call the MakeAnchor and add the AppendChildren function with anchor, so it can receive the anchor info
+    AppendChildren(anchor)
+      
     }
+function makeAnchor(url) { //can call it url as it doesn't matter what you call it because it local
+    //creation of the requested <a> element 
+    const anchor = document.createElement("a")  //create an element template="let/const element = document.createElement(tagName[, options]);"
+    anchor.href = url
+    return anchor
 }
+function AppendChildren(anchor)   {
+    const items = document.querySelector("#items") // ID from HTML, ID better as it is more specific
+    items.appendChild(anchor)
+}
+
