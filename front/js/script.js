47c39f5450dab652a9a290bcb46002b0450f3352
Nasweb113 <104895169+Nasweb113@users.noreply.github.com>
2022-12-08 16:40:03 +0100

    addition of article and image on page

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,9 @@
-//first we FETCH the info
-//using fetch to access URL (used json placeholder template)
-// placeholder text fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => console.log(json))
-
-
-    fetch("http://localhost:3000/api/products") //using fetch to access URL (used json placeholder template)
+fetch("http://localhost:3000/api/products") //using fetch to access URL (used json placeholder template)
     .then((res) => res.json()) // 
-    .then((info) => addProducts(info)) 
+    .then((data) => {
+        console.log(data)
+        addProducts(data) 
+    })
     //it fetches the info and passes by Add products-->
     //it recoveres the info from index 0 in the array 
     //it calls the makeAnchor function and calls the image url and creates the <a></a> anchor element
@@ -14,12 +12,29 @@
     //then it passes by appendChildren and takes the sent element and attaches it to #items element
     //once it finds the id #items, it adds the anchor 
     
+
+
+//***********SOFA DETAILS*************
+//colors": 
+//   "Blue",
+//   "White",
+//    "Black"
+//    "_id": "107fb5b75607497b96722bda5b504926",
+//    "name": "Kanap Sinopé",
+//   "price": 1849,
+//   "imageUrl": "http://localhost:3000/images/kanap01.jpeg",
+//   "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//   "altTxt": "Photo of a blue sofa, two seats"
     
-    function addProducts(info) {    
-    const id = info[0]._id //access the first element in the list of URLs
-    const anchor = makeAnchor(id); //call the MakeAnchor and add the AppendChildren function with anchor, so it can receive the anchor info
-    AppendChildren(anchor)
-      
+    function addProducts(donnes) {    
+    const id = donnes[0]._id //access the first element in the list of URLs
+    const imageUrl = donnes[0].imageUrl 
+    const altTxt = donnes[0].altTxt
+    const image = makeImage(imageUrl, altTxt)
+    const anchor = makeAnchor(id) //call the makeAnchor and add the appendChildren function with anchor, so it can receive the anchor info
+    const article = makeArticle()
+    article.appendChild(image) //append the children to the document
+    appendChildren(anchor, article)
     }
 function makeAnchor(id) { 
     //creation of the requested <a> element 
@@ -27,8 +42,36 @@ function makeAnchor(id) {
     anchor.href = "./product.html?id=42" +id
     return anchor
 }
-function AppendChildren(anchor)   {
+function appendChildren(anchor, article)   {
     const items = document.querySelector("#items") // ID from HTML, ID better as it is more specific
+    if (items !== null) {
     items.appendChild(anchor)
+    anchor.appendChild(article) //article is inside the article
+    console.log("object", items) //test to see if it works
+}
+}
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    return image
+}
+function makeArticle() {
+    const article = document.createElement("article")
+    //const h3 = makeH3()
+    //const p = makeP()
+    //article.appendChild(image)
+    //article.appendChild(h3)
+    //article.appendChild(p)
+    console.log(article)
+    return article
+   
+    
 }
 
+
+
+//problems encountered: IN GENERAL - kept forgetting where and which when appending the children to the document
+//I had to add the anchor to the article, and then the article to the anchor fix: lines 33 and 45
+//I had to add the image to the article, and then the article to the anchor fix: line36
+//had to get the article inside the anchor fix: line 49
